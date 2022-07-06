612300c4247df56b933a0911fab7b75bd36ec7ca
Maxximov <maxximov_yurii@mail.ru>
2022-07-06 19:50:08 +0300

    js auth

--- public/whatauth.js
+++ public/whatauth.js
@@ -1,22 +1,3 @@
-const signInBtn = document.getElementById("signIn");
-const signUpBtn = document.getElementById("signUp");
-const fistForm = document.getElementById("form1");
-const secondForm = document.getElementById("form2");
-const container = document.querySelector(".container");
-
-signInBtn.addEventListener("click", () => {
-	container.classList.remove("right-panel-active");
-});
-
-signUpBtn.addEventListener("click", () => {
-	container.classList.add("right-panel-active");
-});
-
-fistForm.addEventListener("submit", (e) => e.preventDefault());
-secondForm.addEventListener("submit", (e) => e.preventDefault());
-
-
-
 let lest = document.getElementById('signin-form');
 let login = document.querySelector('#us')
 let password = document.querySelector('#pas')
